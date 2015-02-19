66f9b423d90135d4055c5f6005cbd56327c66037
wxu <winnielandau@gmail.com>
2015-02-19 14:03:45 -0800

    fixed #rake fake_data:uqa

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,17 @@ class User < ActiveRecord::Base
   has_many :answers, through: :scorecards
 
   validates :email, :birth_year, :female, :country, presence: true
-  validates :email, uniqueness: true, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+  validates :email, uniqueness: true
+  validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+
   validates :female, inclusion: { in: [true, false] }
 
+  validates :birth_year, numericality: true
   validate :valid_birth_year #validates the field for birth_year, 1910 - 2015, shorter range?
 
-  def valid_birth_years
-    if (birth_year < 1910) && (birth_year > 2015)
-      errors.add(:birth_year, "valid birth year must be between 1920 and 2015!")
+  def valid_birth_year
+    if (birth_year < 1910) && (:birth_year > 2015)
+      errors.add(:birth_year, "valid birth year must be between 1910 and 2015!")
     end
   end
 
