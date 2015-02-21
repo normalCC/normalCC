fbfe8ae2f29e519a43c7166fd0364a34ef98d781
wxu <winnielandau@gmail.com>
2015-02-21 00:51:15 -0800

    add several gems and JS files

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,7 +5,7 @@ class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
   accepts_nested_attributes_for :answers,
                             reject_if: lambda { |x|
-                            x[:title].blank? }
+                            x[:title].blank? }, allow_destroy: true
   validates :answers, presence: true
 
 
