e20e84c82886704648e55fa138625ffcddc2bc43
Hex <unclekel@hotmail.com>
2015-03-03 17:43:27 -0800

    heroku fix

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,8 +1,8 @@
 class AdminUser < ActiveRecord::Base
 
-  has_and_belongs_to_many
-  has_many 
-  has_many 
+ # has_and_belongs_to_many
+ # has_many 
+ # has_many 
 
   FORBIDDEN_USERNAMES = ['littlebopeep','humptydumpty','marymary']
 
