30e93b2f96e0c8824ceb1470f761c5fa5a6fc296
Hex <unclekel@hotmail.com>
2015-03-03 17:46:01 -0800

    heroku fix agian

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,9 +1,5 @@
 class AdminUser < ActiveRecord::Base
 
- # has_and_belongs_to_many
- # has_many 
- # has_many 
-
   FORBIDDEN_USERNAMES = ['littlebopeep','humptydumpty','marymary']
 
   #validates :first_name, :presence => true,
